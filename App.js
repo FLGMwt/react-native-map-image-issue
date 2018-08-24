1d16913d0dc476e57de97252ff49cb6678bfb01e
Ryan Stelly <ryan.stelly@live.com>
2018-08-23 20:32:01 -0500

    add reproducible map issue

--- App.js
+++ App.js
@@ -6,44 +6,84 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import MapView from "react-native-maps";
+import launcher from "./ic_launcher.png";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const styles = StyleSheet.create({
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const ExampleImage = () => (
+  <Image
+    source={launcher}
+    style={{ width: 100, height: 100 }}
+    resizeMode="contain"
+  />
+);
+
+export default class MyApp extends React.Component {
+  state = {
+    isMapReady: false
+  };
+
   render() {
+    const { region } = this.props;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <View style={styles.mapContainer}>
+            <MapView
+              style={styles.map}
+              region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121
+              }}
+              onLayout={() => this.setState({ isMapReady: true })}
+            >
+              {this.state.isMapReady && (
+                <MapView.Marker
+                  coordinate={{
+                    latitude: 37.78825,
+                    longitude: -122.4324
+                  }}
+                >
+                  <MapView.Callout true>
+                    <View>
+                      <Text>foo</Text>
+                      <ExampleImage />
+                      <Text>foo</Text>
+                    </View>
+                  </MapView.Callout>
+                </MapView.Marker>
+              )}
+            </MapView>
+          </View>
+        </View>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>foo</Text>
+          <ExampleImage />
+          <Text>foo</Text>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
